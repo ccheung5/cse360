291b3311bf5a9ccc4fb57723a3ddfc5c5dcf4004
ccheung5 <51342849+ccheung5@users.noreply.github.com>
2019-10-07 17:43:13 -0700

    Adding given file

---
+++ AddingMachine.java
@@ -0,0 +1,36 @@
+package cse360assign2;
+/**
+ * 
+ * @author Celine Cheung
+ *Class ID: CSE 360 - Intro to Software Engineering- Monday 
+* Assignment #2
+ */
+public class AddingMachine
+{
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
