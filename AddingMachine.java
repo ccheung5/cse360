2aa9f2f559691e0579a1493d413b969e1a5cded4
Celine Cheung <ccheung5@asu.edu>
2019-10-07 19:06:12 -0700

    Updated and edit methods of AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,36 +1,45 @@
-package cse360assign2;
+
 /**
- * 
  * @author Celine Cheung
- *Class ID: CSE 360 - Intro to Software Engineering- Monday 
-* Assignment #2
+ * Class ID: CSE 360 - Intro to Software Engineering- Monday 
+ * Assignment #2
+ * Description: Learning to use Git with a simple AddingMachine.java class
  */
 public class AddingMachine
 {
 
 	private int total;
+	private String n = "0";
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
-	public void add (int value) {
+	public void add (int value) 
+	{
+		total = total + value;
+		n = n + " + " + value;
 		
 	}
 	
-	public void subtract (int value) {
-		
+	public void subtract (int value) 
+	{
+		total = total - value;
+		n = n + " - "+ value;
 	}
 		
 	public String toString () {
-		return "";
+		return n;
 	}
 
-	public void clear() {
-	
+	public void clear() 
+	{
+		total= 0;
+		n = "0";
 	}
+
 }
